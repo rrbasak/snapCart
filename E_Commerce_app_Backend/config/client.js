06c9d0d8fc7d0da61a22f3ed532d071ef36f2361
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-06-19 23:28:45 +0530

    Renew redis

--- E_Commerce_app_Backend/config/client.js
+++ E_Commerce_app_Backend/config/client.js
@@ -1,10 +1,15 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  host: "redis-11050.c264.ap-south-1-1.ec2.redns.redis-cloud.com",
-  port: 11050,
-  password: "9x1soWtdn8J6teHOGGZAvVfCrJUgtfpy",
+  host: "redis-18474.c264.ap-south-1-1.ec2.redns.redis-cloud.com",
+  port: 18474,
+  password: "YKIrlL09TrL7QBirut40f73rHB2u1qXA",
 });
+// const redis = new Redis({
+//   host: "redis-11050.c264.ap-south-1-1.ec2.redns.redis-cloud.com",
+//   port: 11050,
+//   password: "9x1soWtdn8J6teHOGGZAvVfCrJUgtfpy",
+// });
 
 redis.on("connect", () => {
   console.log(`Connected to Redis!`.bgMagenta.bgBlue);
