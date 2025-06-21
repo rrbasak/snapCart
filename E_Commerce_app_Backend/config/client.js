64e6aeff355a50ae99c72a07ec053777a6cf643d
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-06-21 12:12:48 +0530

    changed in client

--- E_Commerce_app_Backend/config/client.js
+++ E_Commerce_app_Backend/config/client.js
@@ -1,13 +1,13 @@
 import Redis from "ioredis";
 
-// const redis = new Redis({
-//   host: "redis-18474.c264.ap-south-1-1.ec2.redns.redis-cloud.com",
-//   port: 18474,
-//   password: "YKIrlL09TrL7QBirut40f73rHB2u1qXA",
-// });
-const redis = new Redis(
-  "rediss://default:AWKlAAIjcDFmMjQ5YWRjODEwOWQ0NWYzODRkYzEyZWM4OTI1ZDI4NHAxMA@ready-fox-25253.upstash.io:6379"
-);
+const redis = new Redis({
+  host: "redis-18474.c264.ap-south-1-1.ec2.redns.redis-cloud.com",
+  port: 18474,
+  password: "YKIrlL09TrL7QBirut40f73rHB2u1qXA",
+});
+// const redis = new Redis(
+//   "rediss://default:AWKlAAIjcDFmMjQ5YWRjODEwOWQ0NWYzODRkYzEyZWM4OTI1ZDI4NHAxMA@ready-fox-25253.upstash.io:6379"
+// );
 
 // const redis = new Redis({
 //   host: "redis-11050.c264.ap-south-1-1.ec2.redns.redis-cloud.com",
